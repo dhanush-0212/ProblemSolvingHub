2d8345d0d9b41ce57617f5955ef5b3fb660699b5
Dhanush <161826117+dhanush-0212@users.noreply.github.com>
2025-04-12 07:52:30 +0530

    Time: 43 ms (49.88%) | Memory: 53.1 MB (66.49%) - LeetSync

---
+++ 2732-counter/counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    var x=n;
+    next=0;
+    return function() {
+
+        return x+next++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
