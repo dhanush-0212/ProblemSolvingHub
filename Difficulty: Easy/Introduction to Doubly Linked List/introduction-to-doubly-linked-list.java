df3437426af187b4285240102df1354d525a3dda
Dhanush <161826117+dhanush-0212@users.noreply.github.com>
2025-05-28 20:32:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.java
@@ -0,0 +1,14 @@
+class Solution {
+    Node constructDLL(int arr[]) {
+        if (arr.length == 0) return null;
+        Node head = new Node(arr[0]);
+        Node curr = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node newNode = new Node(arr[i]);
+            curr.next = newNode;
+            newNode.prev = curr;
+            curr = newNode;
+        }
+        return head;
+    }
+}
