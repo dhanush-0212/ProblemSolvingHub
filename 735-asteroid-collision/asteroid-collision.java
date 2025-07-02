f5134a71f252d9e4e5debda21ac90a52cad14d2d
Dhanush <161826117+dhanush-0212@users.noreply.github.com>
2025-07-02 11:24:51 +0530

    Time: 8 ms (17.03%) | Memory: 45.6 MB (20.24%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,30 +1,23 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        
+    public int[] asteroidCollision(int[] arr) {
+       // List<Integer> list=new ArrayList<>();
         Stack<Integer> stck=new Stack<>();
-        for(int i=0;i<asteroids.length;i++){
-           if(asteroids[i]>0) stck.push(asteroids[i]);
-           else{
-             while(!stck.isEmpty() && stck.peek() > 0 
-                                    && stck.peek() < Math.abs(asteroids[i]))
-                {
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0) stck.push(arr[i]);
+            else{
+                while(!stck.isEmpty() && stck.peek() > 0 && stck.peek() < Math.abs(arr[i]))
                     stck.pop();
-                }
-             if(stck.isEmpty()|| stck.peek() <0){
-                stck.push(asteroids[i]);
-             }
-             if(stck.peek()==-asteroids[i]){
-                stck.pop();
-             }
-           }
+                
+                if(stck.isEmpty() || stck.peek()<0) stck.push(arr[i]);
+                if(!stck.isEmpty() && -arr[i] == stck.peek()) stck.pop();
+                
+            }
         }
-       int[] res = new int[stck.size()];
-        int i = stck.size() - 1;
-
-        while(!stck.isEmpty()) {
-            res[i--] = stck.pop();
+        int[] ans=new int[stck.size()];
+        int i=0;
+        for(int num:stck){
+            ans[i++]=num;
         }
-
-        return res;
+        return ans;
     }
 }
