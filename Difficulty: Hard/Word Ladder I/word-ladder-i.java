42c66906f09848a839f7b67e1c3efd2c2a2d55ed
Dhanush <161826117+dhanush-0212@users.noreply.github.com>
2025-07-17 13:46:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Word Ladder I/word-ladder-i.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int wordLadderLength(String startWord, String targetWord,
+                                String[] wordList) {
+        // Code here
+        Set<String> set=new HashSet<>();
+        Queue<pair> q=new ArrayDeque<>();
+
+        for(String word:wordList){
+            set.add(word);
+        }
+        q.add(new pair(startWord,1));
+        set.remove(startWord);
+
+        while(!q.isEmpty()){
+            pair p=q.poll();
+            String word=p.word;
+            int steps=p.steps;
+
+            if(word.equals(targetWord)) return steps;
+
+            for(int i=0;i<word.length();i++){
+                for(char ch='a';ch<='z';ch++){
+                    char[] arr=word.toCharArray();
+                    arr[i]=ch;
+
+                    String changedword=new String(arr);
+
+                    if(set.contains(changedword)){
+                        set.remove(changedword);
+                        q.add(new pair(changedword,steps+1));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
+class pair{
+    String word;
+    int steps;
+    pair(String word,int steps){
+        this.word=word;
+        this.steps=steps;
+    }
+}
