89fbd93d96c5d1a7906fae02d1be3e0872ab6c1f
Dhanush <161826117+dhanush-0212@users.noreply.github.com>
2025-09-21 19:38:10 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (29.06%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,20 +1,38 @@
 class Solution {
     public int trap(int[] arr) {
-        int leftmax=0,rightmax=0,l=0,r=arr.length-1;
-        int total=0;
-        while(l<r){
+        // int n=height.length;
+        // int prefix[]=new int[n];
+        // prefix[0]=height[0];
+        // for(int i=1;i<n;i++) prefix[i]=Math.max(height[i],prefix[i-1]);
 
+        // int suffix[] =new int[n];        
+        // suffix[n-1]=height[n-1];
+        // for(int i=n-2;i>=0;i--) suffix[i]=Math.max(suffix[i+1],height[i]);
+
+        // int total=0;
+        // int leftmax=0;
+        // for(int i=0;i<n;i++){
+        //     leftmax=Math.max(leftmax,height[i]);
+        //     if(height[i]<leftmax && height[i]<suffix[i]){
+        //         total+=Math.min(leftmax,suffix[i])-height[i];
+        //     }
+        // }
+        // return total;
+
+        int lmax=0,rmax=0,tot=0;
+        int l=0,n=arr.length,r=n-1;
+        while(l<r){
             if(arr[l]<arr[r]){
-                if(arr[l]<leftmax) total+=leftmax-arr[l];
-                else leftmax=arr[l];
+                if(lmax>arr[l]) tot+=lmax-arr[l];
+                else lmax=arr[l];
                 l++;
             }
-            else {
-                if(arr[r]< rightmax) total+=rightmax-arr[r];
-                else rightmax=arr[r];
+            else{
+                if(rmax>arr[r]) tot+=rmax-arr[r];
+                else rmax=arr[r];
                 r--;
             }
         }
-        return total;
+        return tot;
     }
 }
