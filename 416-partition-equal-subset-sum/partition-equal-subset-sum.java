af44140a205bec88cf2a9842fea46acc4fa783de
Dhanush <161826117+dhanush-0212@users.noreply.github.com>
2025-10-03 15:38:55 +0530

    Time: 50 ms (46.79%) | Memory: 45.5 MB (67.55%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,18 +1,34 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int totalsum=0;
-        for(int num:nums) totalsum+=num;
-        if (totalsum % 2 != 0) return false;
-        int targetsum=totalsum/2;
-        boolean[] dp=new boolean[targetsum+1];
-        dp[0]=true;
-        for(int num:nums){
-            for(int currsum=targetsum;currsum>=num;currsum--){
-                dp[currsum]=dp[currsum] || dp[currsum-num];
-                if(dp[targetsum]) return true;
+        int totSum=0;
+        for(int num:nums) totSum+=num;
+        if(totSum %2 !=0) return false;
+        int target=totSum/2;
+        return helper(nums,target,nums.length);
+    }
+    public boolean helper(int[] arr,int k,int n){
+        boolean[] prev=new boolean[k+1];
+        
+        prev[0]=true;
+       
+        //for(int i=0;i<n;i++) dp[i][0]=true;
+        
+        if(arr[0] <= k) prev[arr[0]]=true;
+        
+        for(int i=1;i<n;i++){
+            boolean[] curr=new boolean[k+1];
+             curr[0]=true;
+            for(int target=1;target<=k;target++){
+                
+                boolean notpick=prev[target];
+                boolean pick=false;
+                if(arr[i]<=target)
+                    pick=prev[target-arr[i]];
+            
+                curr[target] = pick || notpick;
             }
+            prev=curr;
         }
-        return dp[targetsum];
-
+        return prev[k];
     }
 }
