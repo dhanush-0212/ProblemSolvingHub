dfd636f4033a686487b796433c522dcedf0b30d1
Dhanush <161826117+dhanush-0212@users.noreply.github.com>
2026-08-16 19:01:49 +0530

    Time: 2 ms (96.07%) | Memory: 45.9 MB (18.43%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,19 +1,17 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>(); 
-        helper(candidates,target,candidates.length-1,list,temp);
-        return list;
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(candidates,target,ans,new ArrayList<>(),candidates.length-1);
+        return ans;
     }
-    public void helper(int[] candidates, int target,int idx,List<List<Integer>> list, List<Integer> temp){
-        if(target<0 || idx<0 ) return;
-        if(target==0){
-            list.add(new ArrayList<>(temp));
-            return;
-        }
-        temp.add(candidates[idx]);
-        helper(candidates,target-candidates[idx],idx,list,temp);
-        temp.remove(temp.size()-1);
-        helper(candidates,target,idx-1,list,temp);
+    public void helper(int[] candidates,int target,List<List<Integer>> ans,List<Integer> list,int idx){
+        if(idx<0 || target < 0) return;
+        if(target==0) {ans.add(new ArrayList<>(list)); return;}
+        
+        
+        list.add(candidates[idx]);
+        helper(candidates,target-candidates[idx],ans,list,idx);
+        list.remove(list.size()-1);
+        helper(candidates,target,ans,list,idx-1);
     }
 }
